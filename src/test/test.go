7323c536cdbc5de8db0494d0b17985e7030e99d9
jotitan <titanbar@hotmail.com>
2015-04-14 17:49:02 +0200

    Embed image test

--- src/test/test.go
+++ src/test/test.go
@@ -3,33 +3,42 @@ import (
     "fmt"
     "os"
     "bufio"
+	"encoding/hex"
 )
 
 
 
 func main(){
 
-    path := "D:\\temp\\resize\\IMG_2473_DxO.jpg"
+    path := "src/resources/img.jpg"
+	f,_ := os.Open(path)
 
-    f,_ := os.Open(path)
     defer f.Close()
     buf := bufio.NewReader(f)
     line,_,_ := buf.ReadLine()
     pos := 2
 
+	fmt.Println([]byte("Canon"))
+
     fmt.Println(len(line),line)
-    fmt.Println(string(line))
+    fmt.Println(string(line),"\n\n")
     for  {
         if line[pos]!=255 {
             break
         }
-        marker := line[pos+1]
+		marker := hex.EncodeToString(line[pos:pos+2])
+		switch marker {
+			case "ffe0" : fmt.Println("INF : App marker")
+			case "ffe1" : fmt.Println("INF : Tiff header")
+		}
         length := int(line[pos+2])  + int(line[pos+3])
-        fmt.Println("PRE",marker,length,pos)
+        fmt.Println("PRE",length,pos)
         data := line[pos+4:pos+2+length]
         pos+=2+length
-        fmt.Println("MARKER",marker,pos,string(data))
-        fmt.Println("REST",string(line[pos:]))
-
+        fmt.Println("MARKER ",pos,string(data))
     }
+	line,_,_ = buf.ReadLine()
+	line,_,_ = buf.ReadLine()
+	line,_,_ = buf.ReadLine()
+	fmt.Println("==>",string(line),"\n\n")
 }
